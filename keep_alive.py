8409a858a63ff91546fbb50b0eaa4e7e6eb4c5e6
AmirStPlays <Amirstplays@gmail.com>
2025-06-02 12:16:48 +0330

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,15 +1,14 @@
-from flask import Flask, render_template
-from threading import Thread
+from flask import Flask
+import threading
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return "Alive"
-
-def run():
-    app.run(host='0.0.0.0', port=8082)
+def home():
+    return "OK - bot is alive ✅"
 
 def keep_alive():
-    t = Thread(target=run)
-    t.start()
+    def run():
+        app.run(host="0.0.0.0", port=8080)  # Render پورت 8080 رو می‌خواد
+    thread = threading.Thread(target=run)
+    thread.start()
